dda9403fd4609987a99ca52a3804825baba0f086
Gonzo Knight <gonzalort@outlook.com>
2017-06-26 21:32:49 -0400

    Se programan modulos, se crea super usuario -> usr: root / pass: proyecto2017

--- entidades/models.py
+++ entidades/models.py
@@ -1,11 +1,18 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from django.utils.encoding import python_2_unicode_compatible
 from django.db import models
 
 # Create your models here.
 class tipousuario(models.Model):
 	nombre = models.CharField(max_length=50)
 
+	def __str__(self):
+		return self.nombre
+
 class usuario(models.Model):
 	nombre = models.CharField(max_length=50)
 	tipousuario = models.ForeignKey(tipousuario, null=True, blank=True, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return self.nombre
--- videojuegos/models.py
+++ videojuegos/models.py
@@ -1,18 +1,28 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
-from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+from django.db import models 
 
 class Genero(models.Model):
 	nombre = models.CharField(max_length=50)
 
+	def __str__(self):
+		return self.nombre
+
 class Clasificacion(models.Model):
+	titulo = models.CharField(max_length=50)
 	nombre = models.CharField(max_length=50)
+	descripcion = models.TextField()
+
+	def __str__(self):
+	    return self.titulo
 
 class Juego(models.Model):
 	nombre = models.CharField(max_length=50)
 	descripcion = models.CharField(max_length=144)
-	imagen = models.ImageField(null=True, blank=True)
+	imagen = models.ImageField(null=True, blank=True, upload_to = "static/img")
 	genero = models.ForeignKey(Genero, null=True, blank=True, on_delete=models.CASCADE)
 	clasificacion = models.ForeignKey(Clasificacion, null=True, blank=True, on_delete=models.CASCADE)
-	
+
+	def __str__(self):
+	    return self.nombre
